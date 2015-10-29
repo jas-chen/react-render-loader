6dc375615999506e7ea7469159e9f5392c02f16c
Jas Chen <jas228@gmail.com>
2015-10-29 10:04:25 +0800

    v1.1.0
    add support for React class

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var loaderUtils = require('loader-utils');
-var React = require('react-dom/server');
+var React = require('react');
+var ReactServer = require('react-dom/server');
 
 module.exports = function(content) {
   this.cacheable && this.cacheable();
@@ -9,7 +10,13 @@ module.exports = function(content) {
   var query = loaderUtils.parseQuery(this.query);
 	var fn = query.static ? 'renderToStaticMarkup' : 'renderToString';
 
-  var el = this.exec(content, this.resource);
-  var html = React[fn](el);
-  return "module.exports = '" + html + "';";
+  var classOrElement = this.exec(content, this.resource);
+  var element = typeof classOrElement === 'function' ?
+    React.createElement(classOrElement)
+    : classOrElement;
+
+  var html = ReactServer[fn](element);
+  var result = "module.exports = '" + html + "';";
+  
+  return result;
 };
